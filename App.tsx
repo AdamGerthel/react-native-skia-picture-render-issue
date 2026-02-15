94e6569fe790e798ae0b061802f607af86bea034
Adam Gerthel <insats@gmail.com>
2026-02-15 08:57:59 +0700

    feat: build cache before rendering

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "./src/components/button/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { skiaImageCache } from "./assets/graphics/skia-images";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const [randomValue, setRandomValue] = useState(0);
+
+  useEffect(() => {
+    const buildCache = async () => {
+      await skiaImageCache.generateCache();
+      setIsLoading(false);
+    };
+
+    buildCache();
+  }, []);
 
   const handleSetRandomValue = () => {
     setRandomValue(Math.random());
@@ -12,57 +23,64 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Random value: {randomValue}</Text>
-      <Button
-        key={randomValue + 1}
-        label="Button example 1"
-        onPress={handleSetRandomValue}
-      />
-      <Button
-        key={randomValue + 2}
-        label="Button example 2"
-        onPress={handleSetRandomValue}
-      />
-      <Button
-        key={randomValue + 3}
-        label="Button example 3"
-        onPress={handleSetRandomValue}
-      />
-      <Button
-        key={randomValue + 4}
-        label="Button example 4"
-        onPress={handleSetRandomValue}
-      />
-      <Button
-        key={randomValue + 5}
-        label="Button example 5"
-        onPress={handleSetRandomValue}
-      />
-      <Button
-        key={randomValue + 6}
-        label="Button example 6"
-        onPress={handleSetRandomValue}
-      />
-      <Button
-        key={randomValue + 7}
-        label="Button example 7"
-        onPress={handleSetRandomValue}
-      />
-      <Button
-        key={randomValue + 8}
-        label="Button example 8"
-        onPress={handleSetRandomValue}
-      />
-      <Button
-        key={randomValue + 9}
-        label="Button example 9"
-        onPress={handleSetRandomValue}
-      />
-      <Button
-        key={randomValue + 10}
-        label="Button example 10"
-        onPress={handleSetRandomValue}
-      />
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          <Text>Random value: {randomValue}</Text>
+          <Button
+            key={randomValue + 1}
+            label="Button example 1"
+            onPress={handleSetRandomValue}
+          />
+          <Button
+            key={randomValue + 2}
+            label="Button example 2"
+            onPress={handleSetRandomValue}
+          />
+          <Button
+            key={randomValue + 3}
+            label="Button example 3"
+            onPress={handleSetRandomValue}
+          />
+          <Button
+            key={randomValue + 4}
+            label="Button example 4"
+            onPress={handleSetRandomValue}
+          />
+          <Button
+            key={randomValue + 5}
+            label="Button example 5"
+            onPress={handleSetRandomValue}
+          />
+          <Button
+            key={randomValue + 6}
+            label="Button example 6"
+            onPress={handleSetRandomValue}
+          />
+          <Button
+            key={randomValue + 7}
+            label="Button example 7"
+            onPress={handleSetRandomValue}
+          />
+          <Button
+            key={randomValue + 8}
+            label="Button example 8"
+            onPress={handleSetRandomValue}
+          />
+          <Button
+            key={randomValue + 9}
+            label="Button example 9"
+            onPress={handleSetRandomValue}
+          />
+          <Button
+            key={randomValue + 10}
+            label="Button example 10"
+            onPress={handleSetRandomValue}
+          />
+        </>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
