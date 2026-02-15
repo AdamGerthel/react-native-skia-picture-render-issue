06cbc101428be7d854a151ffa36e3ebc15060e9f
Adam Gerthel <insats@gmail.com>
2026-02-15 09:01:32 +0700

    feat: remove usePictureCache to isolate issue

--- src/components/button/button-background/button-background.tsx
+++ src/components/button/button-background/button-background.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, memo, useEffect } from "react";
+import React, { useRef, memo, useEffect, useMemo } from "react";
 import { PixelRatio, StyleSheet } from "react-native";
 import {
   Canvas,
@@ -14,7 +14,6 @@ import {
   LINE_ORIGINAL_HEIGHT,
   LINE_ORIGINAL_WIDTH,
 } from "../../../../assets/graphics/skia-images";
-import { usePictureCache } from "../../../hooks/usePictureCache";
 
 function getSideVariant(number: number) {
   if (number < 0.3333) {
@@ -45,161 +44,166 @@ const ButtonBackground = memo(function ButtonBackground({
   const sideLeftVariantIndex = getSideVariant(randomNumberA);
   const sideRightVariantIndex = getSideVariant(randomNumberB);
   const roundedSideWidth = PixelRatio.roundToNearestPixel(sideWidth);
-  const picture = usePictureCache(
-    `buttonBackground-${width}-${height}-${roundedSideWidth}-${sideLeftVariantIndex}-${sideRightVariantIndex}-${tint}`,
-    () => {
-      if (!cachedImages) {
-        return;
-      }
+  const picture = useMemo(() => {
+    if (!cachedImages) {
+      return;
+    }
 
-      return createPicture(
-        (canvas) => {
-          const paint = Skia.Paint();
+    return createPicture(
+      (canvas) => {
+        const paint = Skia.Paint();
 
-          paint.setColorFilter(
-            Skia.ColorFilter.MakeMatrix([
-              0,
-              0,
-              0,
-              0,
-              Skia.Color(tint)[0], // Red
-              0,
-              0,
-              0,
-              0,
-              Skia.Color(tint)[1], // Green
-              0,
-              0,
-              0,
-              0,
-              Skia.Color(tint)[2], // Blue
-              0,
-              0,
-              0,
-              1,
-              0, // Alpha
-            ]),
-          );
+        paint.setColorFilter(
+          Skia.ColorFilter.MakeMatrix([
+            0,
+            0,
+            0,
+            0,
+            Skia.Color(tint)[0], // Red
+            0,
+            0,
+            0,
+            0,
+            Skia.Color(tint)[1], // Green
+            0,
+            0,
+            0,
+            0,
+            Skia.Color(tint)[2], // Blue
+            0,
+            0,
+            0,
+            1,
+            0, // Alpha
+          ]),
+        );
 
-          const lineWidth = PixelRatio.roundToNearestPixel(
-            width - roundedSideWidth * 2,
-          );
-          const lineImageWidth = PixelRatio.roundToNearestPixel(
-            LINE_ORIGINAL_WIDTH / 3,
-          );
-          const lineImageHeight = PixelRatio.roundToNearestPixel(
-            LINE_ORIGINAL_HEIGHT / 3,
-          );
+        const lineWidth = PixelRatio.roundToNearestPixel(
+          width - roundedSideWidth * 2,
+        );
+        const lineImageWidth = PixelRatio.roundToNearestPixel(
+          LINE_ORIGINAL_WIDTH / 3,
+        );
+        const lineImageHeight = PixelRatio.roundToNearestPixel(
+          LINE_ORIGINAL_HEIGHT / 3,
+        );
 
-          // Center piece
-          canvas.drawRect(
-            {
-              x: sideWidth,
-              y: lineImageHeight,
-              height: height - lineImageHeight * 2,
-              width: lineWidth,
-            },
-            paint,
-          );
+        // Center piece
+        canvas.drawRect(
+          {
+            x: sideWidth,
+            y: lineImageHeight,
+            height: height - lineImageHeight * 2,
+            width: lineWidth,
+          },
+          paint,
+        );
 
-          // Left side
-          canvas.drawImageRect(
-            cachedImages.buttonLeftSides[sideLeftVariantIndex],
-            {
-              x: 0,
-              y: 0,
-              width: BUTTON_SIDE_ORIGINAL_WIDTH,
-              height: BUTTON_SIDE_ORIGINAL_HEIGHT,
-            },
-            { x: 0, y: 0, width: roundedSideWidth, height },
-            paint,
-          );
+        // Left side
+        canvas.drawImageRect(
+          cachedImages.buttonLeftSides[sideLeftVariantIndex],
+          {
+            x: 0,
+            y: 0,
+            width: BUTTON_SIDE_ORIGINAL_WIDTH,
+            height: BUTTON_SIDE_ORIGINAL_HEIGHT,
+          },
+          { x: 0, y: 0, width: roundedSideWidth, height },
+          paint,
+        );
 
-          // Top Line
-          canvas.save();
-          canvas.clipRect(
-            {
-              x: roundedSideWidth,
-              y: 0,
-              width: lineWidth,
-              height: lineImageHeight,
-            },
-            ClipOp.Intersect,
-            true,
-          );
-          canvas.drawImageRect(
-            cachedImages.lineTop,
-            {
-              x: topOffset,
-              y: 0,
-              width: LINE_ORIGINAL_WIDTH,
-              height: LINE_ORIGINAL_HEIGHT,
-            },
-            {
-              x: roundedSideWidth - topOffset,
-              y: 0,
-              width: lineImageWidth,
-              height: lineImageHeight,
-            },
-            paint,
-          );
-          canvas.restore();
+        // Top Line
+        canvas.save();
+        canvas.clipRect(
+          {
+            x: roundedSideWidth,
+            y: 0,
+            width: lineWidth,
+            height: lineImageHeight,
+          },
+          ClipOp.Intersect,
+          true,
+        );
+        canvas.drawImageRect(
+          cachedImages.lineTop,
+          {
+            x: topOffset,
+            y: 0,
+            width: LINE_ORIGINAL_WIDTH,
+            height: LINE_ORIGINAL_HEIGHT,
+          },
+          {
+            x: roundedSideWidth - topOffset,
+            y: 0,
+            width: lineImageWidth,
+            height: lineImageHeight,
+          },
+          paint,
+        );
+        canvas.restore();
 
-          // Bottom Line
-          canvas.save();
-          canvas.clipRect(
-            {
-              x: roundedSideWidth,
-              y: height - lineImageHeight,
-              width: lineWidth,
-              height: lineImageHeight,
-            },
-            ClipOp.Intersect,
-            true,
-          );
-          canvas.drawImageRect(
-            cachedImages.lineBottom,
-            {
-              x: 0,
-              y: 0,
-              width: LINE_ORIGINAL_WIDTH,
-              height: LINE_ORIGINAL_HEIGHT,
-            },
-            {
-              x: roundedSideWidth - bottomOffset,
-              y: height - lineImageHeight,
-              width: lineImageWidth,
-              height: lineImageHeight,
-            },
-            paint,
-          );
-          canvas.restore();
+        // Bottom Line
+        canvas.save();
+        canvas.clipRect(
+          {
+            x: roundedSideWidth,
+            y: height - lineImageHeight,
+            width: lineWidth,
+            height: lineImageHeight,
+          },
+          ClipOp.Intersect,
+          true,
+        );
+        canvas.drawImageRect(
+          cachedImages.lineBottom,
+          {
+            x: 0,
+            y: 0,
+            width: LINE_ORIGINAL_WIDTH,
+            height: LINE_ORIGINAL_HEIGHT,
+          },
+          {
+            x: roundedSideWidth - bottomOffset,
+            y: height - lineImageHeight,
+            width: lineImageWidth,
+            height: lineImageHeight,
+          },
+          paint,
+        );
+        canvas.restore();
 
-          // Right side
-          canvas.drawImageRect(
-            cachedImages.buttonRightSides[sideRightVariantIndex],
-            {
-              x: 0,
-              y: 0,
-              width: BUTTON_SIDE_ORIGINAL_WIDTH,
-              height: BUTTON_SIDE_ORIGINAL_HEIGHT,
-            },
-            {
-              x: width - roundedSideWidth,
-              y: 0,
-              width: roundedSideWidth,
-              height,
-            },
-            paint,
-          );
-        },
-        {
-          width: PixelRatio.roundToNearestPixel(width),
-          height: PixelRatio.roundToNearestPixel(height),
-        },
-      );
-    },
-  );
+        // Right side
+        canvas.drawImageRect(
+          cachedImages.buttonRightSides[sideRightVariantIndex],
+          {
+            x: 0,
+            y: 0,
+            width: BUTTON_SIDE_ORIGINAL_WIDTH,
+            height: BUTTON_SIDE_ORIGINAL_HEIGHT,
+          },
+          {
+            x: width - roundedSideWidth,
+            y: 0,
+            width: roundedSideWidth,
+            height,
+          },
+          paint,
+        );
+      },
+      {
+        width: PixelRatio.roundToNearestPixel(width),
+        height: PixelRatio.roundToNearestPixel(height),
+      },
+    );
+  }, [
+    cachedImages,
+    width,
+    height,
+    roundedSideWidth,
+    sideLeftVariantIndex,
+    sideRightVariantIndex,
+    tint,
+  ]);
 
   useEffect(() => {
     if (!cachedImages) {
